ce5ee5d61736678805aa1a8912c61f087abf300f
Tony Liu <tliu.auckland@gmail.com>
2015-09-15 14:47:57 -0700

    script can now save all player shot logs

--- playerinfo.py
+++ playerinfo.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python                                                                                                                            
 import requests
 import pandas as pd
+from os import path
 
+DATA_DIR = '/Users/tdliu/nba_analytics/data'
+SEASON = '2014-15'
+SHOT_LOGS_FILENAME = 'shot_logs.csv'
 ''' 
 init function takes in season as a string, e.g. "2014-15"
 self.player_bio_df is a dataframe of stats from the Player Bio page
@@ -23,11 +27,30 @@ class PlayerTracking(object):
         shot_log_url = 'http://stats.nba.com/stats/playerdashptshotlog?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&PlayerID='+ str(player_id) +'&Season=2014-15&SeasonSegment=&SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision='
         response = requests.get(shot_log_url)
         raw_shot_log = response.json()['resultSets'][0]['rowSet']
+        if not raw_shot_log:
+            print "No data available for " + self.player_ids[player_id]
+            return None
         headers = response.json()['resultSets'][0]['headers']
         headers = [x.encode('latin-1') for x in headers]
         shot_log_df = pd.DataFrame(raw_shot_log, columns = headers)
         shot_log_df.insert(0, "PLAYER_NAME", self.player_ids[player_id])
         shot_log_df.insert(0, "PLAYER_ID", player_id)
         return shot_log_df
-    
 
+# This function takes in PlayerTracking object and returns a data frame with shot logs for all players    
+def get_all_player_shot_logs(player_tracking):
+    player_ids = player_tracking.player_ids
+    frames = []
+    for player_id in player_ids:
+        player_shot_log = player_tracking.get_player_shot_log(player_id)
+        if player_shot_log is None:
+            continue
+        frames.append(player_shot_log)
+    return pd.concat(frames)
+        
+def main():
+    player_tracking = PlayerTracking(SEASON)
+    shot_logs_df = get_all_player_shot_logs(player_tracking)
+    shot_logs_df.to_csv(path.join(DATA_DIR, '_'.join([SEASON, SHOT_LOGS_FILENAME])), index = False)
+
+if __name__ == "__main__": main()
